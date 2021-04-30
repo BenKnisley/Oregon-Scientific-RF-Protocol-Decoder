34630a97c7cbe99d500e004494360513dbb98a26
BenKnisley <benknisley@gmail.com>
2021-04-30 13:54:26 -0400

    Updated Executable used in start_realtime

--- start_realtime.sh
+++ start_realtime.sh
@@ -3,4 +3,4 @@
 ## Date: 6 Jan, 2019
 
 ## Start reciving, convert audio signal, and port to processing
-rtl_fm -g 36.0  -f 433.85300M -M am -s 48k -E deemp - | sox -r 48000 -t raw -b 16 -c 1 -e signed-integer /dev/stdin -r 48000 -t wav -b 16 -c 1 -e signed-integer - | ./exe4.py
+rtl_fm -g 36.0  -f 433.85300M -M am -s 48k -E deemp - | sox -r 48000 -t raw -b 16 -c 1 -e signed-integer /dev/stdin -r 48000 -t wav -b 16 -c 1 -e signed-integer - | ./realtime_decode.py
